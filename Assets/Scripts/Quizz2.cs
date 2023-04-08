445bbf6e4420751b7181864567e5d55f6d4656b3
chii12 <scy17032@gmail.com>
2023-04-08 12:23:38 +0900

    1st commit

---
+++ Assets/Scripts/Quizz2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Quizz2 : MonoBehaviour
+{
+    public Transform player;　// Harukoモデルを格納した変数
+
+    void Update()
+    {
+ 
+        if (Input.GetMouseButtonDown(0)) // 左クリックされたら
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                player.position = hit.point;
+
+                // 接触したオブジェクトがTerrain Colliderだった場合
+                if (hit.collider is TerrainCollider)
+                {
+                    // 接触点からのTerrainの法線ベクトルを取得
+                    Vector3 terrainNormal = hit.normal;
+
+                    // Harukoの上方向を調整して垂直に立つようにする
+                    player.up = terrainNormal;
+                }
+            }
+        }
+    }
+}
